5db49d09840b145562e018f4de82ad32f5b8eafb
Duh5569 <wakaw5302@gmail.com>
2024-09-04 11:58:07 -0300

    Usando classe fornecedor

--- SysPecNSLib/Fornecedor.cs
+++ SysPecNSLib/Fornecedor.cs
@@ -8,5 +8,31 @@ namespace SysPecNSLib
 {
     public class Fornecedor
     {
+        public int Id { get; set; }
+
+        public string? Razao_Social { get; set; }
+
+        public string? Fantasia { get; set; }
+
+        public string? Cnpj { get; set; }
+
+        public string? Contato { get; set; }
+
+        public string? Telefone { get; set; }
+
+        public string? Email { get; set; }
+
+
+        public Fornecedor(){}
+
+        public Fornecedor(int id, string? razao_social, string? fantasia, string? cnpj, string? contato, string? telefone, string? email)
+        {
+            id = Id;
+            razao_social = Razao_Social;
+            fantasia = Fantasia;
+                
+
+
+        }
     }
 }
