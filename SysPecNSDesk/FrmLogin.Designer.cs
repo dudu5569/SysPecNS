8097e1e4783b644f0b493087fcdb5964cc23ce77
Duh5569 <wakaw5302@gmail.com>
2024-09-12 09:05:45 -0300

    endereço funcionando

--- SysPecNSDesk/FrmLogin.Designer.cs
+++ SysPecNSDesk/FrmLogin.Designer.cs
@@ -89,6 +89,8 @@
             txtEmail.Name = "txtEmail";
             txtEmail.Size = new Size(230, 23);
             txtEmail.TabIndex = 4;
+            txtEmail.Text = "eduardorich@gmail.com";
+
             // 
             // txtSenha
             // 
